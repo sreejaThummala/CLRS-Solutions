978d589066bcf633e8b5ba62578aba2402b2487b
Sreeja Thummala <sthummala@wisc.edu>
2016-07-30 18:51:24 -0700

    Create moveZeros.java

---
+++ moveZeros.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int ptr = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] !=0)
+            {
+                nums[ptr++] = nums[i];
+            }
+        }
+        
+        for(int i = ptr ; i <nums.length; i++)
+        {
+            nums[i] = 0;
+        }
+        
+    }
+}
