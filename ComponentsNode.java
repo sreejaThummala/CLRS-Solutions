4680dcc2ac0d5f8a57bd53b726184b38f3fb2cc2
Sreeja Thummala <sthummala@wisc.edu>
2016-08-06 21:55:43 -0700

    II attempt

--- ComponentsNode.java
+++ ComponentsNode.java
@@ -11,15 +11,16 @@ class GFG {
 		    t--;
 		    int n = in.nextInt();
 		    int m = in.nextInt();
-		    boolean[][] graph = new boolean[n][n];
+		    boolean[][] graph = new boolean[n+1][n+1];
 		    
+		    int[] edges = new int[n+1];
 		    for(int i =0; i < m ; i++)
 		    {
 		        int u = in.nextInt();
 		        int v = in.nextInt();
 		        
-		        graph[u-1][v-1] = true;
-		 
+		        graph[u][v] = true;
+		        graph[v][u] = true;
 		    }
 		    
 		    System.out.println(getConnectingNodes(graph,n));
@@ -28,55 +29,46 @@ class GFG {
 	
 	private static int getConnectingNodes(boolean[][] graph, int n)
 	{
+	    Boolean[] visited  = new Boolean[n+1];
 	    
-	    Map<Integer, Set<Integer>> components = new HashMap<Integer, Set<Integer>>();
-	    
-	    for(int u = 0; u < n ; u++) {
-	        for(int v = 0; v < n ;v++){
-	            if(graph[u][v]){
-	                Set<Integer> uComponent = components.get(u);
-	                Set<Integer> vComponent = components.get(v);
-	                
-	                if(uComponent == null){
-	                    uComponent = new HashSet<Integer>();
-	                }
-	                
-	                if(vComponent == null){
-	                    vComponent = new HashSet<Integer>();
-	                }
-	                
-	                if( u <v){
-	                    uComponent.add(v);
-	                    uComponent.addAll(vComponent);
-	                    components.put(u,uComponent);
-	                    components.remove(v);
-	                }else{
-	                    vComponent.add(v);
-	                    vComponent.addAll(uComponent);
-	                    components.put(v,vComponent);
-	                    components.remove(u);
-	                }
-
+	    Arrays.fill(visited, false);
+	    int sum = 0;
+	    for(int i = 1; i <= 2; i++){
+	                if(!visited[i]){
+	                    Integer[] componentEdges = new Integer[n+1];
+	                    Arrays.fill(componentEdges,0);
+	                    dfs(visited, graph,i, componentEdges,n);
+	                    
+	                    int[] evenOdd = new int[2];
+	                    for(int j = 1; j<=n; j++){
+	                        evenOdd[componentEdges[j]%2]++;
+	                    }
+	                    
+	                    sum += evenOdd[1];
+	                    
+	                    if(evenOdd[1] ==0){
+	                        sum += 2;
+	                    }
 	            }
 	        }
+	        
+	        return sum/2;
+	    
+	    
+	}
+	
+	private static void dfs(Boolean[] visited, boolean[][] graph, int u, Integer[] componentEdges,
+	int n){
+	    
+	    if(visited[u]) return;
+	    
+	    for(int i = u+1; i <=n ; i++){
+	        if(graph[i][u]){
+	            dfs(visited, graph,i, componentEdges, n);
+	            componentEdges[i]++;
+	        }
 	    }
 	    
-	    int sum =0;
-	    Iterator<Map.Entry<Integer,Set<Integer>>> it = components.entrySet().iterator();
-	    while(it.hasNext()){
-	        
-	        int componentSize = it.next().getValue().size();
-	        
-	        if(componentSize%2==0){
-	            sum += 2;
-	        }
-	        
-	        if(componentSize%2 ==1){
-	            sum += componentSize;
-	        }
-	    }
-	    
-	    return (sum)/2;
-	    
+	    visited[u] = true;
 	}
 }
