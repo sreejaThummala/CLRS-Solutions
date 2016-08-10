3678855e2b6124c5543c8e47e400730fe8507ec6
Sreeja Thummala <sthummala@wisc.edu>
2016-08-09 19:29:06 -0700

    Update Range Addition.java

--- Leetcode/Range Addition.java
+++ Leetcode/Range Addition.java
@@ -4,3 +4,20 @@ Each operation is representated as triplet [startIdx, endIdx, inc] which increme
 each element of subarry of [startIdx, endIdx] with ic;
 Return the modified arary after all the k operations were execute
 **/
+public class Solution{
+  public int[] getModifiedArray(int len ,int[][] updates){
+    int[] res = new int[len];
+    for(int[] update: updates){
+      res[update[0]] += update[2];
+      if(update[1]+1 < len) res[updates[1]+1] = -updates[2];
+    }
+    
+    int value = 0;
+    for(int i = 0; i < len; i++){
+      value += res[i];
+      res[i] = value;
+    }
+    
+    return res;
+  }
+}
