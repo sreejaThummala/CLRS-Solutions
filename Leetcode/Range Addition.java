3a0415ebc36aebd46096c5a50d4059edb193972c
Sreeja Thummala <sthummala@wisc.edu>
2016-08-09 18:56:54 -0700

    Create Range Addition.java

---
+++ Leetcode/Range Addition.java
@@ -0,0 +1,6 @@
+/**
+Assume you have an array of length n initialized all 0s and are given k update operations.
+Each operation is representated as triplet [startIdx, endIdx, inc] which increments
+each element of subarry of [startIdx, endIdx] with ic;
+Return the modified arary after all the k operations were execute
+**/
