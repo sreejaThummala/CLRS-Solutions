cc1c7e6026b48f4d2f13b9cfd41290b89338d658
Sreeja Thummala <sthummala@wisc.edu>
2016-08-10 06:59:23 -0700

    Update Integer Break I .java

--- Leetcode/Integer Break I .java
+++ Leetcode/Integer Break I .java
@@ -1,11 +1,19 @@
-public class Solution{
-
-  public int integerBreak(int n ){
-    int[] dp = new int[n+1];
+public class Solution {
+    public int integerBreak(int n) {
+    int[] dp = new int[Math.max(7,n+1)];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 2;
+    dp[4] = 4;
+    dp[5] = 6;
+    dp[6] = 9;
+    
     for(int i = 7; i <= n; i++){
       dp[i] = 3*dp[i-3];
     }
     
     return dp[n];
-  }
+        
+    }
 }
