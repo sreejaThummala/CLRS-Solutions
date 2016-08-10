34610f19f1c41af8d991b9050032c2d0e0a9e516
Sreeja Thummala <sthummala@wisc.edu>
2016-08-10 11:35:41 -0700

    Create MissingNumber.java

---
+++ Leetcode/MissingNumber.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum  = n*(n+1)/2;
+        
+        int _sum = 0;
+        for(int i = 0; i <n; i++){
+            _sum += nums[i];
+        }
+        
+        return sum - _sum;
+        
+    }
+}
