706fc615e151b9c3c39902e4949470a0b7f71284
Sreeja Thummala <sthummala@wisc.edu>
2016-08-10 23:10:27 -0700

    Update Find K Pairs with smallest sums.java

--- Leetcode/Find K Pairs with smallest sums.java
+++ Leetcode/Find K Pairs with smallest sums.java
@@ -1,4 +1,3 @@
-//works only when elements are positive
 public class Solution {
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         int[][] neighbors = {{0,1},{1,0}};
@@ -11,24 +10,25 @@ public class Solution {
         if(n == 0 || m == 0) return res;
         
         boolean[][] visited = new boolean[n][m];
-        int maxSum = nums1[n-1]+nums2[m-1]+1;
+        int minSum = nums1[0]+nums2[0];
         int coord = 0*m + 0;
-        Queue<Integer> queue = new PriorityQueue<Integer>();
-        queue.offer(coord*maxSum+nums1[0]+nums2[0]);
+        int maxCoord = (n+1)*m;
+        Queue<Long> queue = new PriorityQueue<Long>();
+        queue.offer(Long.valueOf(coord)+Long.valueOf((nums1[0]+nums2[0]-minSum))*maxCoord);
         while(k>0 && !queue.isEmpty()){
-            int minHash = queue.poll();
-            int coordHash = minHash/maxSum;
-            int _xmin = coordHash/m;
-            int _ymin = coordHash%m;
-            res.add(new int[]{nums1[_xmin], nums2[_ymin]});
+            long minHash = queue.poll();
+            long coordHash = minHash%maxCoord;
+            long _xmin = coordHash/m;
+            long _ymin = coordHash%m;
+            res.add(new int[]{nums1[(int)_xmin], nums2[(int)_ymin]});
             k--;
             for(int[] neighbor : neighbors){
-                int _nx = _xmin+neighbor[0];
-                int _ny = _ymin+neighbor[1];
-                if(_nx>=n || _ny>=m || visited[_nx][_ny]) continue;
-                visited[_nx][_ny] = true;
-                int _ncoord = _nx*m + _ny;
-                queue.offer(_ncoord*maxSum+nums1[_nx]+nums2[_ny]);
+                long _nx = _xmin+neighbor[0];
+                long _ny = _ymin+neighbor[1];
+                if(_nx>=n || _ny>=m || visited[(int)_nx][(int)_ny]) continue;
+                visited[(int)_nx][(int)_ny] = true;
+                long _ncoord = _nx*m + _ny;
+                queue.offer(_ncoord+(Long.valueOf(nums1[(int)_nx]+nums2[(int)_ny])-Long.valueOf(minSum))*maxCoord);
             }
             
         }
