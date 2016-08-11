a23ed7fe9efc8b387f25cf8502cd7ca84ae7be54
Sreeja Thummala <sthummala@wisc.edu>
2016-08-11 08:48:07 -0700

    Create kthlargest.java

---
+++ Leetcode/kthlargest.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+        for(int i : nums){
+           q.offer(i);
+           if(q.size()>k) q.poll();
+        }
+        
+        return q.peek();
+        
+    }
+}
