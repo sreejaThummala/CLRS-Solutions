7e1b21b147e8b68b1bbde1d51668db3b7978a0a4
Sreeja Thummala <sthummala@wisc.edu>
2016-08-10 23:52:50 -0700

    Create Sudoku solver.cpp

---
+++ Leetcode/Sudoku solver.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+bool isValidSudoku(vector<vector<char>>& board) {
+        int r_len = board.size();
+        if(r_len == 0) return true;
+        int c_len = board[0].size();
+        if(r_len != c_len) return false;
+        if( r_len != 9) return false;
+        
+        for(int j=0; j < 9 ; j++) {
+                        int n_c_mask = 0;
+
+            for(int i = 0; i<9; i++){
+                  if(board[i][j] != '.')
+                {
+                    if(n_c_mask & (1<<(board[i][j] - '1'))) return false;
+                     n_c_mask  = n_c_mask | (1 << (board[i][j] -'1'));
+                }
+            }
+        }
+        
+        for(int i = 0; i<9; i++) {
+            int n_r_mask = 0;
+            for(int j = 0; j<9; j++){
+                if(board[i][j] != '.')
+                {
+                    if(n_r_mask & (1<<(board[i][j] - '1'))) return false;
+                     n_r_mask  = n_r_mask | ( 1 << (board[i][j] -'1'));
+
+                }
+
+            }
+        }
+        
+        for(int i = 0 ;i<3; i++) {
+            for(int j = 0; j<3; j++) {
+                 int n_m_mask = 0;
+                for(int k = i*3; k < ((i*3)+3) ; k++) {
+                    for(int h = j*3; h < ((j*3)+3); h++){
+                         if(board[k][h] != '.' && board[k][h] != '0' )
+                {
+                    if(n_m_mask & (1<<(board[k][h] - '1'))) return false;
+                     n_m_mask  = n_m_mask | (1 << (board[k][h] -'1'));
+
+                }
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    bool _solveSudoku(vector<vector<char>>& board, int row, int col) {
+        if(row >= 9) return true;
+
+        if(col>=9) return _solveSudoku(board, row+1, 0);
+        if(board[row][col]!='.') return  _solveSudoku(board, row, col+1);
+        
+        for(int i = 1; i<=9; i++)
+        {
+            board[row][col] = (char)(i + '0');
+            if(isValidSudoku(board)){
+                if( _solveSudoku(board, row, col+1)) return true;
+            }
+               
+        }
+            board[row][col] ='.';
+        
+        
+        return false;
+    
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        
+        _solveSudoku(board,0,0);
+    }
+};
