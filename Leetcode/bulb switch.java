c08e4fd21d8d7842c52c08c6fda76f770b6a22d3
Sreeja Thummala <sthummala@wisc.edu>
2016-08-13 12:25:38 -0700

    Create bulb switch.java

---
+++ Leetcode/bulb switch.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);  //need odd number times to toggle. only possible if odd divisors. only possible in square numbers
+        
+    }
+}
