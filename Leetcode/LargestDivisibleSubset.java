82ba65d3884f3da0a8bfee3569deb4bceb73dbf6
Sreeja Thummala <sthummala@wisc.edu>
2016-08-13 17:23:10 -0700

    Update LargestDivisibleSubset.java

--- Leetcode/LargestDivisibleSubset.java
+++ Leetcode/LargestDivisibleSubset.java
@@ -1,32 +1,32 @@
-public List<Integer> largeDivisibleSubset(int[] nums){
-  Arrays.sort(nums);
-  int n = nums.length;
-  int[] dp = = new int[n];
-  Arrays.fill(dp,1);
-  
-  int[] pre = new int[n];
-  int res_idx = 0;
-  int res = -1;
-  Arrays.fill(pre,0);
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j<i; j++){
-      if(nums[i]%nums[j] ==0 && dp[j]+1 > dp[i]){
-        dp[i] = dp[j]+1;
-        pre[i] = j;
-      }
-      if(res < dp[i]){
-        res = dp[i];
-        res_idx = i;
-      }
+public class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+      List<Integer> res=new ArrayList<>();
+        if(nums.length == 0) return res;
+        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxLength = 1, maxIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        map.put(nums[i], nums[j]);
+                    }
+                    if (dp[i] > maxLength) {
+                        maxLength = dp[i];
+                        maxIndex = i;
+                    }
+                }
+            }
+        }
+        int key = nums[maxIndex];
+        res.add(key);
+        while (map.containsKey(key)) {
+            res.add(map.get(key));
+            key = map.get(key);
+        }
+        return res;
     }
-  }
-  
-  
-  List<Integer> ans = new ArrayList<>();
-  while(pre[res_idx] != -1){
-    ans.add(nums[res_idx];
-    res_idx = pre[res_idx];
-  }
-  
-  return ans;
 }
