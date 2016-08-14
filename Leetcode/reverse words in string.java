e0a6bb80466a15365fb1e5eb34f07b71e417466f
Sreeja Thummala <sthummala@wisc.edu>
2016-08-13 18:05:10 -0700

    Create reverse words in string.java

---
+++ Leetcode/reverse words in string.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public String reverseWords(String s) {
+        if (s == null || s.length() == 0) {
+			return "";
+		}
+ 
+		// split to words by space
+		String[] arr = s.split(" ");
+		StringBuilder sb = new StringBuilder();
+		for (int i = arr.length - 1; i >= 0; --i) {
+			if (!arr[i].equals("")) {
+				sb.append(arr[i]).append(" ");
+			}
+		}
+		return sb.toString().trim();
+
+    }
+}
