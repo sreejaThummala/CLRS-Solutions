558b90efae10c7735df01fbb827800ae6603f39c
Sreeja Thummala <sthummala@wisc.edu>
2016-08-15 00:14:30 -0700

    Create minimum size subarry sum.java

---
+++ Leetcode/minimum size subarry sum.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int cur = 0;
+        int start = 0;
+        int end = 0;
+        while(true){
+            if(cur<s){
+                if(end == nums.length) break;
+                cur += nums[end++];
+            }
+            else{
+                min = Math.min(min, end-start); //no need for start boundary check
+                //end will always be less than start
+                cur -= nums[start++];
+            }
+        }
+        
+        return min == Integer.MAX_VALUE? 0: min;
+    }
+}
