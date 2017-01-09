bcecc50102a4b385180876775d140cb6f4fec479
Sreeja Thummala <sthummala@wisc.edu>
2017-01-09 02:44:49 -0800

    Update ConvexPolygon.java

--- Leetcode/ConvexPolygon.java
+++ Leetcode/ConvexPolygon.java
@@ -1,8 +1,8 @@
 public class Solution {
     public boolean isConvex(List<List<Integer>> points) {
         int n = points.size();
-        int cur = 0;
-        int prev = 0;
+        long cur = 0;
+        long prev = 0;
         for(int i = 0; i < n; i++){
             int dx1 = points.get((i+1)%n).get(0) - points.get(i).get(0);
             int dy1 = points.get((i+1)%n).get(1) - points.get(i).get(1);
@@ -10,11 +10,9 @@ public class Solution {
             int dy2 = points.get((i+2)%n).get(1) - points.get(i).get(1);
             
             cur = ((dx1 * dy2) - (dx2 * dy1));
-            if(cur != 0){
-                if(prev*cur < 0) return false;
-               
-            }
-             prev = cur;
+            if(cur == 0) continue;
+            if(prev*cur < 0) return false;
+            prev = cur;
             
         }
         
